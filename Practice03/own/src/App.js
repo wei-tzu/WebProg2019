b75d518afc2e73e0c3961056cf170258bb941fb8
Angela <b05705039@ntu.edu.tw>
2019-03-24 20:50:16 +0800

    homework03

--- Practice03/own/src/App.js
+++ Practice03/own/src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 class Website extends Component {
   render() {
     return (
-      <div id="colorlib-page">
-        <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
-        <aside id="colorlib-aside" role="complementary" class="border js-fullheight">
-          <h1 id="colorlib-logo"><a href="index.html">Balay</a></h1>
-          <nav id="colorlib-main-menu" role="navigation">
-            <ul>{this.props.menu.map( e => <li><a>{e}</a></li>)}</ul>
-          </nav>
-        </aside>
-      </div>
+        <div id="colorlib-page">
+          <a href="#" className="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+          <aside id="colorlib-aside" role="complementary" className="border js-fullheight">
+            <h1 id="colorlib-logo"><a href="index.html">Balay</a></h1>
+            <nav id="colorlib-main-menu" role="navigation">
+              <ul>{this.props.menu.map( e => <li><a>{e}</a></li>)}</ul>
+            </nav>
+          </aside>
+        </div>
     );
   }
 }
